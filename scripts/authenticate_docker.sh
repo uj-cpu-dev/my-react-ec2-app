fe7889fe8cfb7d90c7a88808ea02bfd28c64716d
uj-cpu-dev <134852095+uj-cpu-dev@users.noreply.github.com>
2024-12-07 13:21:35 +0000

    test-full-pipeline

--- scripts/authenticate_docker.sh
+++ scripts/authenticate_docker.sh
@@ -3,7 +3,7 @@
 echo "Authenticating Docker with ECR..."
 AWS_REGION="us-east-1"  # Update to your AWS region
 ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-REPO_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/frontend/react-app"
+REPO_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/my-react-app"
 
 # Authenticate Docker with ECR
 aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${REPO_URI}"
--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,10 +4,21 @@
 AWS_REGION="us-east-1"  # Update to your AWS region
 ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 REPO_URI="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
-IMAGE_NAME="frontend/react-app"  # Update to your ECR repository name
+IMAGE_NAME="my-react-app"  # Update to your ECR repository name
 IMAGE_TAG=$(cat /home/ec2-user/app/image_tag.txt)
 CONTAINER_NAME="react-app-container"
 PORT="3000"
+
+# Check if Docker is installed
+echo "Checking if Docker is installed..."
+if ! command -v docker &> /dev/null; then
+  echo "Docker is not installed. Installing Docker..."
+  sudo yum update -y
+  sudo amazon-linux-extras enable docker
+  sudo yum install -y docker
+  sudo systemctl enable docker
+  echo "Docker installed successfully."
+fi
 
 # Check if Docker is running
 echo "Checking Docker daemon status..."
@@ -25,7 +36,7 @@ fi
 
 # Pull the Docker image from ECR
 echo "Pulling the Docker image from ECR..."
-docker pull "${REPO_URI}/${IMAGE_NAME}:feature-branch-${IMAGE_TAG}"
+docker pull "${REPO_URI}/${IMAGE_NAME}:${IMAGE_TAG}"
 if [ $? -ne 0 ]; then
   echo "Failed to pull the image from ECR."
   exit 1
@@ -33,7 +44,7 @@ fi
 
 # Run the container
 echo "Running the Docker container..."
-docker run -d --name "${CONTAINER_NAME}" -p ${PORT}:${PORT} "${REPO_URI}/${IMAGE_NAME}:feature-branch-${IMAGE_TAG}"
+docker run -d --name "${CONTAINER_NAME}" -p ${PORT}:${PORT} "${REPO_URI}/${IMAGE_NAME}:${IMAGE_TAG}"
 if [ $? -ne 0 ]; then
   echo "Failed to start the container."
   exit 1
